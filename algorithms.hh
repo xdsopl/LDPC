4dc97d68bf8efc0215f68193c116e5bbebb0e7ea
Ahmet Inan <xdsopl@gmail.com>
2018-11-25 05:06:35 +0100

    added min-sum-c algorithm

--- algorithms.hh
+++ algorithms.hh
@@ -126,4 +126,119 @@ struct MinSumAlgorithm<SIMD<int8_t, WIDTH>, UPDATE>
 	}
 };
 
+template <typename TYPE, typename UPDATE, int FACTOR>
+struct MinSumCAlgorithm
+{
+	static TYPE one()
+	{
+		return vdup<TYPE>(1);
+	}
+	static TYPE sign(TYPE a, TYPE b)
+	{
+		return vsign(a, b);
+	}
+	static TYPE correction_factor(TYPE a, TYPE b)
+	{
+		TYPE apb = vabs(vadd(a, b));
+		TYPE apb2 = vadd(apb, apb);
+		TYPE amb = vabs(vsub(a, b));
+		TYPE amb2 = vadd(amb, amb);
+		TYPE factor2 = vdup<TYPE>(FACTOR * 2);
+		typedef typename TYPE::value_type VALUE;
+		auto pc = vmask(vdup<TYPE>(VALUE(FACTOR) / VALUE(2)));
+		auto nc = vmask(vdup<TYPE>(-VALUE(FACTOR) / VALUE(2)));
+		pc = vand(pc, vand(vcgt(factor2, apb), vcgt(amb, apb2)));
+		nc = vand(nc, vand(vcgt(factor2, amb), vcgt(apb, amb2)));
+		return vreinterpret<TYPE>(vorr(pc, nc));
+	}
+	static TYPE minc(TYPE a, TYPE b)
+	{
+		TYPE m = vmin(vabs(a), vabs(b));
+		TYPE x = vsign(vsign(m, a), b);
+		x = vadd(x, correction_factor(a, b));
+		return x;
+	}
+	static void finalp(TYPE *links, int cnt)
+	{
+		TYPE tmp[cnt];
+		CODE::exclusive_reduce(links, tmp, cnt, minc);
+		for (int i = 0; i < cnt; ++i)
+			links[i] = tmp[i];
+	}
+	static TYPE add(TYPE a, TYPE b)
+	{
+		return vadd(a, b);
+	}
+	static bool bad(TYPE v, int blocks)
+	{
+		auto tmp = vcgtz(v);
+		for (int i = 0; i < blocks; ++i)
+			if (!tmp.v[i])
+				return true;
+		return false;
+	}
+	static TYPE update(TYPE a, TYPE b)
+	{
+		return UPDATE::update(a, b);
+	}
+};
+
+template <int WIDTH, typename UPDATE, int FACTOR>
+struct MinSumCAlgorithm<SIMD<int8_t, WIDTH>, UPDATE, FACTOR>
+{
+	typedef SIMD<int8_t, WIDTH> TYPE;
+	static TYPE one()
+	{
+		return vdup<TYPE>(1);
+	}
+	static TYPE sign(TYPE a, TYPE b)
+	{
+		return vsign(a, b);
+	}
+	static TYPE correction_factor(TYPE a, TYPE b)
+	{
+		TYPE apb = vqabs(vqadd(a, b));
+		TYPE apb2 = vqadd(apb, apb);
+		TYPE amb = vqabs(vqsub(a, b));
+		TYPE amb2 = vqadd(amb, amb);
+		TYPE factor2 = vdup<TYPE>(FACTOR * 2);
+		typedef typename TYPE::value_type VALUE;
+		auto pc = vmask(vdup<TYPE>(VALUE(FACTOR) / VALUE(2)));
+		auto nc = vmask(vdup<TYPE>(-VALUE(FACTOR) / VALUE(2)));
+		pc = vand(pc, vand(vcgt(factor2, apb), vcgt(amb, apb2)));
+		nc = vand(nc, vand(vcgt(factor2, amb), vcgt(apb, amb2)));
+		return vreinterpret<TYPE>(vorr(pc, nc));
+	}
+	static TYPE minc(TYPE a, TYPE b)
+	{
+		TYPE m = vmin(vqabs(a), vqabs(b));
+		TYPE x = vsign(vsign(m, a), b);
+		x = vqadd(x, correction_factor(a, b));
+		return x;
+	}
+	static void finalp(TYPE *links, int cnt)
+	{
+		TYPE tmp[cnt];
+		CODE::exclusive_reduce(links, tmp, cnt, minc);
+		for (int i = 0; i < cnt; ++i)
+			links[i] = tmp[i];
+	}
+	static TYPE add(TYPE a, TYPE b)
+	{
+		return vqadd(a, b);
+	}
+	static bool bad(TYPE v, int blocks)
+	{
+		auto tmp = vcgtz(v);
+		for (int i = 0; i < blocks; ++i)
+			if (!tmp.v[i])
+				return true;
+		return false;
+	}
+	static TYPE update(TYPE a, TYPE b)
+	{
+		return UPDATE::update(a, b);
+	}
+};
+
 #endif
