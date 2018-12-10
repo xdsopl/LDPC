33af448f7e9eeaf4c692935d56f784148706e685
Ahmet Inan <xdsopl@gmail.com>
2018-12-10 15:44:05 +0100

    added fake parity bit to use only one kernel

--- layered_decoder.hh
+++ layered_decoder.hh
@@ -27,110 +27,7 @@ class LDPCDecoder
 	}
 	bool bad(TYPE *data, TYPE *parity, int blocks)
 	{
-		{
-			int cnt = cnc[0];
-			int offset[cnt], shift[cnt];
-			for (int c = 0; c < cnt; ++c) {
-				shift[c] = pos[c] % M;
-				offset[c] = pos[c] - shift[c];
-			}
-			{
-				int msg_pos[cnt];
-				for (int c = 0; c < cnt; ++c) {
-					msg_pos[c] = offset[c] + shift[c];
-					shift[c] = (shift[c] + 1) % M;
-				}
-				TYPE cnv = alg.sign(alg.one(), parity[0]);
-				for (int c = 0; c < cnt; ++c)
-					cnv = alg.sign(cnv, data[msg_pos[c]]);
-				if (alg.bad(cnv, blocks))
-					return true;
-			}
-			for (int j = 1; j < M; ++j) {
-				int msg_pos[cnt];
-				for (int c = 0; c < cnt; ++c) {
-					msg_pos[c] = offset[c] + shift[c];
-					shift[c] = (shift[c] + 1) % M;
-				}
-				TYPE cnv = alg.sign(alg.sign(alg.one(), parity[j+(q-1)*M-1]), parity[j]);
-				for (int c = 0; c < cnt; ++c)
-					cnv = alg.sign(cnv, data[msg_pos[c]]);
-				if (alg.bad(cnv, blocks))
-					return true;
-			}
-		}
-		for (int i = 1; i < q; ++i) {
-			int cnt = cnc[i];
-			int offset[cnt], shift[cnt];
-			for (int c = 0; c < cnt; ++c) {
-				shift[c] = pos[CNL*i+c] % M;
-				offset[c] = pos[CNL*i+c] - shift[c];
-			}
-			for (int j = 0; j < M; ++j) {
-				int msg_pos[cnt];
-				for (int c = 0; c < cnt; ++c) {
-					msg_pos[c] = offset[c] + shift[c];
-					shift[c] = (shift[c] + 1) % M;
-				}
-				TYPE cnv = alg.sign(alg.sign(alg.one(), parity[M*(i-1)+j]), parity[M*i+j]);
-				for (int c = 0; c < cnt; ++c)
-					cnv = alg.sign(cnv, data[msg_pos[c]]);
-				if (alg.bad(cnv, blocks))
-					return true;
-			}
-		}
-		return false;
-	}
-	void update(TYPE *data, TYPE *parity)
-	{
-		TYPE *bl = bnl;
-		{
-			int cnt = cnc[0];
-			int offset[cnt], shift[cnt];
-			for (int c = 0; c < cnt; ++c) {
-				shift[c] = pos[c] % M;
-				offset[c] = pos[c] - shift[c];
-			}
-			{
-				int msg_pos[cnt];
-				for (int c = 0; c < cnt; ++c) {
-					msg_pos[c] = offset[c] + shift[c];
-					shift[c] = (shift[c] + 1) % M;
-				}
-				int deg = cnt + 1;
-				TYPE inp[deg], out[deg];
-				for (int c = 0; c < cnt; ++c)
-					inp[c] = out[c] = alg.sub(data[msg_pos[c]], bl[c]);
-				inp[cnt] = out[cnt] = alg.sub(parity[0], bl[cnt]);
-				alg.finalp(out, deg);
-				for (int c = 0; c < cnt; ++c)
-					data[msg_pos[c]] = alg.add(inp[c], out[c]);
-				parity[0] = alg.add(inp[cnt], out[cnt]);
-				for (int d = 0; d < deg; ++d)
-					alg.update(bl++, out[d]);
-			}
-			int deg = cnt + 2;
-			for (int j = 1; j < M; ++j) {
-				int msg_pos[cnt];
-				for (int c = 0; c < cnt; ++c) {
-					msg_pos[c] = offset[c] + shift[c];
-					shift[c] = (shift[c] + 1) % M;
-				}
-				TYPE inp[deg], out[deg];
-				for (int c = 0; c < cnt; ++c)
-					inp[c] = out[c] = alg.sub(data[msg_pos[c]], bl[c]);
-				inp[cnt] = out[cnt] = alg.sub(parity[j+(q-1)*M-1], bl[cnt]);
-				inp[cnt+1] = out[cnt+1] = alg.sub(parity[j], bl[cnt+1]);
-				alg.finalp(out, deg);
-				for (int c = 0; c < cnt; ++c)
-					data[msg_pos[c]] = alg.add(inp[c], out[c]);
-				parity[j+(q-1)*M-1] = alg.add(inp[cnt], out[cnt]);
-				parity[j] = alg.add(inp[cnt+1], out[cnt+1]);
-				for (int d = 0; d < deg; ++d)
-					alg.update(bl++, out[d]);
-			}
-		}
-		for (int i = 1; i < q; ++i) {
+		for (int i = 0; i < q; ++i) {
 			int cnt = cnc[i];
 			int deg = cnt + 2;
 			TYPE buf[deg*M];
@@ -142,8 +39,49 @@ class LDPCDecoder
 				for (int j = 0; j < M - shift; ++j)
 					buf[M*c+j] = data[offset+shift+j];
 			}
+			if (i) {
+				for (int j = 0; j < M; ++j)
+					buf[M*cnt+j] = parity[M*(i-1)+j];
+			} else {
+				buf[M*cnt] = alg.one();
+				for (int j = 1; j < M; ++j)
+					buf[M*cnt+j] = parity[j+(q-1)*M-1];
+			}
 			for (int j = 0; j < M; ++j)
-				buf[M*cnt+j] = parity[M*(i-1)+j];
+				buf[M*(cnt+1)+j] = parity[M*i+j];
+			for (int j = 0; j < M; ++j) {
+				TYPE cnv = alg.sign(alg.one(), buf[j]);
+				for (int d = 1; d < deg; ++d)
+					cnv = alg.sign(cnv, buf[M*d+j]);
+				if (alg.bad(cnv, blocks))
+					return true;
+			}
+		}
+		return false;
+	}
+	void update(TYPE *data, TYPE *parity)
+	{
+		TYPE *bl = bnl;
+		for (int i = 0; i < q; ++i) {
+			int cnt = cnc[i];
+			int deg = cnt + 2;
+			TYPE buf[deg*M];
+			for (int c = 0; c < cnt; ++c) {
+				int shift = pos[CNL*i+c] % M;
+				int offset = pos[CNL*i+c] - shift;
+				for (int j = 0; j < shift; ++j)
+					buf[M*c+j+(M-shift)] = data[offset+j];
+				for (int j = 0; j < M - shift; ++j)
+					buf[M*c+j] = data[offset+shift+j];
+			}
+			if (i) {
+				for (int j = 0; j < M; ++j)
+					buf[M*cnt+j] = parity[M*(i-1)+j];
+			} else {
+				buf[M*cnt] = alg.one();
+				for (int j = 1; j < M; ++j)
+					buf[M*cnt+j] = parity[j+(q-1)*M-1];
+			}
 			for (int j = 0; j < M; ++j)
 				buf[M*(cnt+1)+j] = parity[M*i+j];
 			for (int j = 0; j < M; ++j) {
@@ -164,8 +102,13 @@ class LDPCDecoder
 				for (int j = 0; j < M - shift; ++j)
 					data[offset+shift+j] = buf[M*c+j];
 			}
-			for (int j = 0; j < M; ++j)
-				parity[M*(i-1)+j] = buf[M*cnt+j];
+			if (i) {
+				for (int j = 0; j < M; ++j)
+					parity[M*(i-1)+j] = buf[M*cnt+j];
+			} else {
+				for (int j = 1; j < M; ++j)
+					parity[j+(q-1)*M-1] = buf[M*cnt+j];
+			}
 			for (int j = 0; j < M; ++j)
 				parity[M*i+j] = buf[M*(cnt+1)+j];
 		}
@@ -205,7 +148,7 @@ public:
 			}
 			ldpc->next_bit();
 		}
-		LT = ldpc->links_total();
+		LT = ldpc->links_total() + 1;
 		delete ldpc;
 		bnl = reinterpret_cast<TYPE *>(aligned_alloc(sizeof(TYPE), sizeof(TYPE) * LT));
 		pty = reinterpret_cast<TYPE *>(aligned_alloc(sizeof(TYPE), sizeof(TYPE) * R));
